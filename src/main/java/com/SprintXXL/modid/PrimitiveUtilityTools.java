0c6ce4e62679c33223b46c704c5802df487b9655
SprintXXL <mspmain@pm.me>
2026-06-06 08:55:25 +0200

    init commit

---
+++ src/main/java/com/SprintXXL/modid/PrimitiveUtilityTools.java
@@ -0,0 +1,17 @@
+package com.SprintXXL.modid;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import static com.SprintXXL.primitiveutilitytools.Reference.*;
+
+@Mod(
+        modid = MODID,
+        name = NAME,
+        version = VERSION)
+public class PrimitiveUtilityTools {
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+}
