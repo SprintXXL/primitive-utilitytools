55db51ace72acce18037345adc4bcd30dbc82fe7
SprintXXL <mspmain@pm.me>
2026-07-11 00:26:32 +0200

    .

--- src/main/java/com/SprintXXL/primitiveutilitytools/tools/items/wrappers/ItemWrench.java
+++ src/main/java/com/SprintXXL/primitiveutilitytools/tools/items/wrappers/ItemWrench.java
@@ -2,10 +2,19 @@ package com.SprintXXL.primitiveutilitytools.tools.items.wrappers;
 
 import com.SprintXXL.primitiveutilitytools.tools.items.UtilityToolBase;
 import com.SprintXXL.primitiveutilitytools.tools.tooltype.ToolType;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class ItemWrench extends UtilityToolBase {
 
     public ItemWrench() {
         super("wrench", ToolType.WRENCH);
+    }
+
+    @Override
+    public boolean doesSneakBypassUse(ItemStack stack, IBlockAccess world, BlockPos pos, EntityPlayer player) {
+        return true;
     }
 }
